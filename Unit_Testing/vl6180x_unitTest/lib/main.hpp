006740edf613eff55177667dc008268d9bc9512e
Mahmoud Samy <mahmoudsamyzx11@gmail.com>
2024-08-29 00:54:58 +0300

    Update Test Code Files

---
+++ Unit_Testing/vl6180x_unitTest/lib/main.hpp
@@ -0,0 +1,12 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+#include <Arduino.h>
+#include <Wire.h>
+#include <VL6180X.h>
+
+#define ON_BOARD_LED        PC13
+
+void vl6SensorConfig(void);
+
+#endif /* _MAIN_H_ */
