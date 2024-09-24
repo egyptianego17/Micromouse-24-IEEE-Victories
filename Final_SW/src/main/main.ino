c6092efe1b4993ecdc47ee521708a23d6e9426bd
egyptianego17 <abdulrahman.omar17h@gmail.com>
2024-09-24 05:12:04 +0300

    update main code

--- Final_SW/src/main/main.ino
+++ Final_SW/src/main/main.ino
@@ -6,17 +6,15 @@
 #define MOTOR_R_2 PB14
 #define MOTOR_L_1 PB9
 #define MOTOR_L_2 PB8
-#define ROTATE_ANGLE 81.8
-const int MOTOR_R_ENC_A = PA2; // Right encoder pin A
-const int MOTOR_R_ENC_B = PA3; // Right encoder pin B
-const int MOTOR_L_ENC_A = PA4; // Left encoder pin A
-const int MOTOR_L_ENC_B = PA5; // Left encoder pin B
-
+#define ROTATE_ANGLE 90
+const int MOTOR_R_ENC_A = PA2;
+const int MOTOR_R_ENC_B = PA3; 
+const int MOTOR_L_ENC_A = PA4; 
+const int MOTOR_L_ENC_B = PA5; 
 volatile float currentYaw = 0.0; // Global variable to store the yaw angle
 
 // Task handle for the angle updating task
 TaskHandle_t updateAngleTaskHandle;
-
 
 // Wheel circumference in cm
 const float wheelCircumference = 10.362; // cm
@@ -27,50 +25,50 @@ volatile uint32_t encoderA_counter = 0; // Right encoder
 volatile uint32_t encoderB_counter = 0; // Left encoder
 
 // Global variables
-double targetYaw = 90.0;   // Target yaw for the next turn (90-degree increments)
-double yaw = 0;            // Current yaw in degrees
-double rateOfIncrease = 0; // Rate of increase in yaw during initialization
+double targetYaw = 90.0;   
+double yaw = 0;           
+double rateOfIncrease = 0; 
 bool initialized = false;
-double totalYawIncrease = 0; // Total yaw increase during initialization
+double totalYawIncrease = 0;
 double desiredAngle = 0;
+
 // Time variables
 long previousTime = 0;
 long initStartTime = 0;
-long initDuration = 10000; // 10 seconds in milliseconds
+long initDuration = 10000;
 
 // PID constants
-float Kp = 2.0;  // Proportional gain
-float Ki = 0.1;  // Integral gain
-float Kd = 0.5;  // Derivative gain
+float Kp = 2.0;  
+float Ki = 0.1; 
+float Kd = 0.5;  
 
-// PID variables
 float error = 0.0;
 float previous_error = 0.0;
 float integral = 0.0;
 float derivative = 0.0;
 float control_output = 0.0;
 
-
-// Function prototypes
 void initGyroRate();
 void turnRight();
 void turnLeft();
 void stopMotors();
-void turnRightM(int speed);
-void turnLeftM(int speed);
+void turnRightM();
+void turnLeftM();
 void moveForward(void);
 void stopMotors(void);
 float calculateTotalDistance();
 float calculate_distance(uint32_t pulse_count);
 void updateAngleTask(void* pvParameters);
+void rightEncoderCb();
+void leftEncoderCb();
+void left(void);
+void right(void);
 void setup() {
   Serial.begin(115200);
 
-  // Initialize IMU
   IMU::init();
   IMU::read();
 
-  // Motor pins setup
   pinMode(MOTOR_R_1, OUTPUT);
   pinMode(MOTOR_R_2, OUTPUT);
   pinMode(MOTOR_L_1, OUTPUT);
@@ -84,67 +82,37 @@ void setup() {
   
   attachInterrupt(digitalPinToInterrupt(MOTOR_L_ENC_A), leftEncoderCb, CHANGE);
   attachInterrupt(digitalPinToInterrupt(MOTOR_L_ENC_B), leftEncoderCb, CHANGE);
-  // Initialize gyroscope rate
   initGyroRate();
-  // create print task
   xTaskCreate(updateAngleTask,
     "Task1",
     configMINIMAL_STACK_SIZE + 50,
     NULL,
     tskIDLE_PRIORITY + 2,
     &updateAngleTaskHandle);
-  // start FreeRTOS
   vTaskStartScheduler();
 
-  // should never return
   Serial.println(F("Die"));
-  // while(1);
 }
 
 void loop() {
-  // Example usage: turn 90 degrees to the right
-  // turnRight();
-  turnLeft();
-  abs(currentYaw - desiredAngle);
+  left();
+  Serial.println(abs(currentYaw - desiredAngle));
   delay(2000);
-  // Serial.print("Current Yaw: ");
-  // Serial.println(currentYaw);
-
-  // moveForward();
-  // turnLeft();
-  // turnLeft();
-  // turnLeft();
-  // turnLeft();
-  // stopMotors();
-  // delay(2000);
-  // moveForward();
-  // turnLeft();
-  // turnLeft();
-  // turnLeft();
-  // // Pause for a moment before the next action
-  // delay(2000);
-
-  // // Example usage: turn 90 degrees to the left
-  // turnLeft();
-
-  // // Pause for a moment before the next action
-  // delay(2000);
+  right();
+  delay(2000);
 }
 
 void updateAngleTask(void *pvParameters) {
   UNUSED(pvParameters);
   previousTime = micros();
 
-  // Flash led every 200 ms.
   for (;;) {
-    // Read IMU data
     IMU::read();
     
     long currentTime = micros();
-    double dt = (currentTime - previousTime) / 1000000.0; // Convert to seconds
+    double dt = (currentTime - previousTime) / 1000000.0; 
     previousTime = currentTime;
 
-    // Get Z-axis gyro data and calculate yaw
     double gyroZ = IMU::getRawGyroZ() / 131.0;
     currentYaw += (gyroZ - rateOfIncrease) * dt;
     // Ensure the yaw is in the 360-degree range (optional, depending on your use case)
@@ -158,54 +126,46 @@ void updateAngleTask(void *pvParameters) {
   }
 }
 
-
 void left(void) {
-  desiredAngle += 90;
+  desiredAngle += ROTATE_ANGLE;
   turnLeft();
 }
 
-void moveForwardM(int speed) {
-  analogWrite(MOTOR_R_1, 120);
-  analogWrite(MOTOR_R_2, 0);
-  analogWrite(MOTOR_L_1, 120);
-  analogWrite(MOTOR_L_2, 0);
+void right(void) {
+  desiredAngle -= ROTATE_ANGLE;
+  turnRight();
 }
 
-/**
- * Turn 90 degrees to the right.
- */
+void moveForwardM() {
+  digitalWrite(MOTOR_R_1, 1);
+  digitalWrite(MOTOR_R_2, 0);
+  digitalWrite(MOTOR_L_1, 1);
+  digitalWrite(MOTOR_L_2, 0);
+}
+
 void turnRight() {
   if (!initialized) {
     Serial.println("Gyroscope not initialized.");
     return;
   }
 
-  yaw = 0; // Reset yaw to 0 for this turn
   previousTime = micros();
-
-  while (abs(yaw)-90.0 < 1) {
-    IMU::read();
-    long currentTime = micros();
-    double dt = (currentTime - previousTime) / 1000000.0;
-    previousTime = currentTime;
-
-    double gyroZ = IMU::getRawGyroZ() / 131.0;
-    yaw += (gyroZ - rateOfIncrease) * dt;
-
-    // Set motors to turn right
-    turnRightM(120 );
-    // Output current yaw for debugging
-    Serial.print("Turning Right. Current Yaw: ");
-    Serial.println(yaw);
+  while (abs(currentYaw - desiredAngle) > 0.3) {
+    Serial.println("abs(currentYaw - desiredAngle)");
+    Serial.println(abs(currentYaw - desiredAngle)); 
+    Serial.println("yaw < desiredAngle - 0.3");
+    Serial.println(currentYaw < desiredAngle);
+    if (currentYaw < desiredAngle - 0.3) {
+      Serial.print("Left");
+      turnRightM();
+    }
+    else if (currentYaw > desiredAngle + 0.3) {
+      Serial.print("Right");
+      turnLeftM();
+    }
   }
-
-  stopMotors();
-  Serial.println("Completed 90-degree right turn.");
 }
 
-/**
- * Turn 90 degrees to the left.
- */
 void turnLeft() {
   if (!initialized) {
     Serial.println("Gyroscope not initialized.");
@@ -214,21 +174,20 @@ void turnLeft() {
 
   previousTime = micros();
   while (abs(currentYaw - desiredAngle) > 0.3) {
+    Serial.println("abs(currentYaw - desiredAngle)");
     Serial.println(abs(currentYaw - desiredAngle)); 
-    // Check yaw and adjust motor control accordingly
-    if (yaw < desiredAngle - 0.3) {
-      turnLeftM(120);
+    Serial.println("yaw < desiredAngle - 0.3");
+    Serial.println(currentYaw < desiredAngle);
+    if (currentYaw < desiredAngle - 0.3) {
+      Serial.print("Left");
+      turnLeftM();
     }
-    else if (yaw > desiredAngle + 0.3) {
-      turnRightM(120);
+    else if (currentYaw > desiredAngle + 0.3) {
+      Serial.print("Right");
+      turnRightM();
     }
-
-    // Output current yaw for debugging
-    Serial.print("Adjusting turn. Current Yaw: ");
-    Serial.println(yaw);
   }
 
-  // Stop the motors once yaw is within acceptable range
   stopMotors();
   Serial.println("Completed 90-degree turn.");
 }
@@ -246,34 +205,29 @@ void leftEncoderCb() {
 }
 
 
-// Function to calculate the distance based on encoder pulses
 float calculate_distance(uint32_t pulse_count) {
-  float revolutions = (float)(pulse_count) / 200;  // Assuming 200 pulses per revolution
-  return revolutions * wheelCircumference;  // Distance in cm
+  float revolutions = (float)(pulse_count) / 200; 
+  return revolutions * wheelCircumference; 
 }
 
-/**
- * Initialize the gyroscope rate by calculating drift during a fixed time interval.
- */
+
 void initGyroRate() {
   initStartTime = millis();
   previousTime = micros();
   totalYawIncrease = 0;
   
-  // Collect data to calculate the rate of yaw increase (gyro drift)
   while (millis() - initStartTime < initDuration) {
     IMU::read();
     long currentTime = micros();
-    double dt = (currentTime - previousTime) / 1000000.0; // Time in seconds
+    double dt = (currentTime - previousTime) / 1000000.0;
     previousTime = currentTime;
 
-    double gyroZ = IMU::getRawGyroZ() / 131.0; // Assuming 131 LSB/°/s
+    double gyroZ = IMU::getRawGyroZ() / 131.0;
 
     totalYawIncrease += gyroZ * dt;
   }
 
-  // Calculate rate of increase based on the total yaw drift during the initialization period
-  double elapsedTime = (millis() - initStartTime) / 1000.0; // Convert milliseconds to seconds
+  double elapsedTime = (millis() - initStartTime) / 1000.0; 
   rateOfIncrease = totalYawIncrease / elapsedTime;
   initialized = true;
 
@@ -282,57 +236,47 @@ void initGyroRate() {
   Serial.println(rateOfIncrease);
 }
 
-// Function to encapsulate PID control and movement logic
 void moveForward(void) {
   int target_distance = 17;
-  // Calculate the current distance traveled
   encoderA_counter = 0;
   encoderB_counter = 0;
   int current_distance = 0;
-  // Check if the car has reached the target distance (20 cm)
   while (current_distance < target_distance) {
     float current_distance = calculate_distance((encoderA_counter + encoderB_counter) / 2);
 
-    // Apply the motor speed to move forward
-    moveForwardM(100);
+    moveForwardM();
 
-    // Print current distance and motor speed for debugging
     Serial.print("Distance: ");
     Serial.print(current_distance);
     Serial.print(" cm | Motor Speed: ");
   }
 }
 
-/**
- * Stop both motors.
- */
 void stopMotors() {
-  analogWrite(MOTOR_R_1, LOW);
-  analogWrite(MOTOR_R_2, LOW);
-  analogWrite(MOTOR_L_1, LOW);
-  analogWrite(MOTOR_L_2, LOW);
-  Serial.println("Motors stopped.");
+  digitalWrite(MOTOR_R_1, LOW);
+  digitalWrite(MOTOR_R_2, LOW);
+  digitalWrite(MOTOR_L_1, LOW);
+  digitalWrite(MOTOR_L_2, LOW);
 }
 
-void turnLeftM(int speed)
+void turnLeftM()
 {
-  analogWrite(MOTOR_R_1, 0  );
-  analogWrite(MOTOR_R_2, speed);
-  analogWrite(MOTOR_L_2, speed);
-  analogWrite(MOTOR_L_1, 0);
+  digitalWrite(MOTOR_R_1, 1);
+  digitalWrite(MOTOR_R_2, 0);
+  digitalWrite(MOTOR_L_2, 1);
+  digitalWrite(MOTOR_L_1, 0);
 }
 
 
-void turnRightM(int speed)
+void turnRightM()
 {
-  analogWrite(MOTOR_R_1, speed);
-  analogWrite(MOTOR_R_2, 0);
-  analogWrite(MOTOR_L_2, 0);
-  analogWrite(MOTOR_L_1, speed);
+  digitalWrite(MOTOR_R_1, 0);
+  digitalWrite(MOTOR_R_2, 1);
+  digitalWrite(MOTOR_L_2, 0);
+  digitalWrite(MOTOR_L_1, 1);
 }
 
 float calculateTotalDistance() {
     float encoder_ticks = (encoderA_counter + encoderB_counter) / 2.0;
     return encoder_ticks * distance_per_tick;
 }
-
